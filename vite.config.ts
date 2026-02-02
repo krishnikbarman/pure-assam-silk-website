f3d88bc01b28ca15af726968a757e7ff3944aa07
RyukGriezeman <krishnikbarman12@gmail.com>
2026-02-03 02:46:09 +0530

    Configure deployment from master branch using docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/pure-assam-silk-website/' : '/',
+export default defineConfig({
+  base: '/',
   plugins: [react()],
-}))
+  build: {
+    outDir: 'docs'
+  }
+})
