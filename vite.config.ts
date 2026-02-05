bdb5d4a1ebe85ced87b0643c30e8c626ff147de8
RyukGriezeman <krishnikbarman12@gmail.com>
2026-02-05 20:38:17 +0530

    Fix TypeScript errors for environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/pure-assam-silk-website/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/pure-assam-silk-website/' : '/',
   plugins: [react()],
   build: {
     outDir: 'dist'
   }
-})
+}))
